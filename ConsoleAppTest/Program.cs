59019b358268baf3a37b0f51734d0fcb0b35d227
Fejanos <fenyvesijani97@gmail.com>
2024-11-14 19:25:59 +0100

    Modified ConsoleWriteLine on the Website

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Github!");
+            Console.WriteLine("Hello Pulled Github");
         }
     }
 }
